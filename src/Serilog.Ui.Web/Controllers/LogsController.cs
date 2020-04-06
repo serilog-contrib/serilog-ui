a55333c388a068a96b4f7898f870ad92333650c6
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-04-06 20:12:24 +0430

    Add log table.

--- src/Serilog.Ui.Web/Controllers/LogsController.cs
+++ src/Serilog.Ui.Web/Controllers/LogsController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Serilog.Ui.Core;
 using Serilog.Ui.Web.ViewModel;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Serilog.Ui.Web.Controllers
 {
     public class LogsController : Controller
     {
+        private static string _scripts;
+        private static string _styles;
         private readonly IDataProvider _dataProvider;
 
         public LogsController(IDataProvider dataProvider)
@@ -23,7 +27,36 @@ namespace Serilog.Ui.Web.Controllers
                 Logs = logs
             };
 
+            GetResources();
+
             return View(viewModel);
+        }
+
+        private void GetResources()
+        {
+            if (_styles != null)
+            {
+                ViewData["Styles"] = _styles;
+                ViewData["Scripts"] = _scripts;
+                return;
+            }
+
+            _styles = SetResource("Serilog.Ui.Web.wwwroot.css.main.css");
+            ViewData["Styles"] = _styles;
+
+            _scripts = SetResource("Serilog.Ui.Web.wwwroot.js.main.js");
+            ViewData["Scripts"] = _scripts;
+        }
+
+        private string SetResource(string resourceName)
+        {
+            var resourceStream = GetType().Assembly.GetManifestResourceStream(resourceName);
+            var resource = string.Empty;
+
+            using var reader = new StreamReader(resourceStream, Encoding.UTF8);
+            resource += reader.ReadToEnd();
+
+            return resource;
         }
     }
 }
---
+++ src/Serilog.Ui.Web/wwwroot/js/main.js
@@ -0,0 +1,15 @@
+﻿(function ($) {
+    "use strict";
+
+    var fullHeight = function () {
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+    };
+    fullHeight();
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+})(jQuery);
