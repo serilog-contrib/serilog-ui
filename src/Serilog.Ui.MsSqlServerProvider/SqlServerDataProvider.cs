311a38c157ad84641c88eb773f4ca4f61d45278c
Mohsen Esmailpour <mo.esmp@gmail.com>
2021-01-10 12:38:24 +0330

    Replace System.Data.SqlClient with Microsoft.Data.SqlClient.

--- src/Serilog.Ui.MsSqlServerProvider/SqlServerDataProvider.cs
+++ src/Serilog.Ui.MsSqlServerProvider/SqlServerDataProvider.cs
@@ -1,8 +1,8 @@
 ﻿using Dapper;
+using Microsoft.Data.SqlClient;
 using Serilog.Ui.Core;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 
