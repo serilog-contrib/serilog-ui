6c35224e9f6b5b760efa56cb380137e0d868b235
Mohsen Esmailpour <mo.esmp@gmail.com>
2021-04-17 23:02:22 +0430

    Add XML document to data provider and options builder interfaces.

--- src/Serilog.Ui.Core/IDataProvider.cs
+++ src/Serilog.Ui.Core/IDataProvider.cs
@@ -4,8 +4,21 @@ using System.Threading.Tasks;
 
 namespace Serilog.Ui.Core
 {
+    /// <summary>
+    /// Data provider interface
+    /// </summary>
     public interface IDataProvider
     {
+        /// <summary>
+        /// Fetches the log data asynchronous.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="count">The count.</param>
+        /// <param name="level">The log level to filter log.</param>
+        /// <param name="searchCriteria">The search criteria to filter log.</param>
+        /// <param name="startDate">The start date to filter log.</param>
+        /// <param name="endDate">The end date to filter log.</param>
+        /// <returns>Task&lt;System.ValueTuple&lt;IEnumerable&lt;LogModel&gt;, System.Int32&gt;&gt;.</returns>
         Task<(IEnumerable<LogModel>, int)> FetchDataAsync(
             int page,
             int count,
--- src/Serilog.Ui.Core/OptionsBuilder/ISerilogUiOptionsBuilder.cs
+++ src/Serilog.Ui.Core/OptionsBuilder/ISerilogUiOptionsBuilder.cs
@@ -2,8 +2,15 @@
 
 namespace Serilog.Ui.Core
 {
+    /// <summary>
+    /// Serilog UI options builder
+    /// </summary>
     public interface ISerilogUiOptionsBuilder
     {
+        /// <summary>
+        /// Gets the services collection.
+        /// </summary>
+        /// <value>The services.</value>
         IServiceCollection Services { get; }
     }
 }
