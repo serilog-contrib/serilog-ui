dcc7b0d02866d2f5e663355e5ebca0bf290b5cb1
Mohsen Esmailpour <mo.esmp@gmail.com>
2021-10-06 16:46:21 +0400

    Fix #10.

--- src/Serilog.Ui.MongoDbProvider/Extensions/SerilogUiOptionBuilderExtensions.cs
+++ src/Serilog.Ui.MongoDbProvider/Extensions/SerilogUiOptionBuilderExtensions.cs
@@ -5,8 +5,53 @@ using System;
 
 namespace Serilog.Ui.MongoDbProvider
 {
+    /// <summary>
+    /// SerilogUI option builder extensions.
+    /// </summary>
     public static class SerilogUiOptionBuilderExtensions
     {
+        /// <summary>
+        /// Adds MongoDB log data provider.
+        /// </summary>
+        /// <param name="optionsBuilder">The options builder.</param>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="collectionName">Name of the collection.</param>
+        /// <exception cref="ArgumentNullException">connectionString</exception>
+        /// <exception cref="ArgumentNullException">collectionName</exception>
+        public static void UseMongoDb(
+            this SerilogUiOptionsBuilder optionsBuilder,
+            string connectionString,
+            string collectionName
+        )
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                throw new ArgumentNullException(nameof(connectionString));
+
+            if (string.IsNullOrEmpty(collectionName))
+                throw new ArgumentNullException(nameof(collectionName));
+
+            var mongoProvider = new MongoDbOptions
+            {
+                ConnectionString = connectionString,
+                DatabaseName = MongoUrl.Create(connectionString).DatabaseName,
+                CollectionName = collectionName
+            };
+
+            ((ISerilogUiOptionsBuilder)optionsBuilder).Services.AddSingleton(mongoProvider);
+            ((ISerilogUiOptionsBuilder)optionsBuilder).Services.AddSingleton<IMongoClient>(o => new MongoClient(connectionString));
+            ((ISerilogUiOptionsBuilder)optionsBuilder).Services.AddScoped<IDataProvider, MongoDbDataProvider>();
+        }
+
+        /// <summary>
+        /// Adds MongoDB log data provider.
+        /// </summary>
+        /// <param name="optionsBuilder">The options builder.</param>
+        /// <param name="connectionString">The connection string without database name.</param>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <param name="collectionName">Name of the collection.</param>
+        /// <exception cref="ArgumentNullException">connectionString</exception>
+        /// <exception cref="ArgumentNullException">databaseName</exception>
+        /// <exception cref="ArgumentNullException">collectionName</exception>
         public static void UseMongoDb(
             this SerilogUiOptionsBuilder optionsBuilder,
             string connectionString,
