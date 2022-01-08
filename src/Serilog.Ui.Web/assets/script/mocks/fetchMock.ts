59a352653d31139e9d06094fcae987c68f7ad729
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2022-01-08 22:21:38 +0100

    add serilog typings, mock search params

--- src/Serilog.Ui.Web/assets/script/mocks/fetchMock.ts
+++ src/Serilog.Ui.Web/assets/script/mocks/fetchMock.ts
@@ -1,10 +1,61 @@
-﻿import { rest } from 'msw'
+﻿import { isAfter, parseJSON } from 'date-fns/esm';
+import compareAsc from 'date-fns/esm/fp/compareAsc/index';
+import isBefore from 'date-fns/esm/fp/isBefore/index';
+import parseISO from 'date-fns/esm/parseISO';
+import compareDesc from 'date-fns/fp/compareDesc/index';
+import { rest } from 'msw'
+import { EncodedSeriLogObject, LogLevel, SearchParameters } from '../../types/types';
+import { fakeLogs } from './samples';
 
 export const handlers = [
-    // Handles a GET /user request
     rest.get('https://127.0.0.1:1234/api/logs', (req, res, ctx) => {
-        console.log('here')
-        return res(ctx.status(200), ctx.json([]))
+        const params = getSearchParams(req.url.searchParams);
+        const logs = fakeLogs.logs.filter(byLevel(LogLevel[params.level]))
+            .filter(byDates(params.start, params.end))
+            .filter(bySearch(params.search))
+            .sort(byDirection(params.sort));
+        const sliceValues = applyLimits(params.count, params.page);
+
+        const data = {
+            currentPage: params.page,
+            count: params.count,
+            total: logs.length,
+            logs: logs.slice(sliceValues.start, sliceValues.end)
+        };
+        return res(ctx.status(200), ctx.json(data))
     }),
 ];
 
+const getSearchParams = (params: URLSearchParams) => ({
+    count: Number.parseInt(params.get(SearchParameters.Count)),
+    page: Number.parseInt(params.get(SearchParameters.Page)),
+    level: params.get(SearchParameters.Level),
+    search: params.get(SearchParameters.Search),
+    start: params.get(SearchParameters.StartDate),
+    end: params.get(SearchParameters.EndDate),
+    sort: params.get(SearchParameters.SortDirection),
+});
+
+const byLevel = (level?: LogLevel) =>
+    (item: EncodedSeriLogObject) => level ? item.level === level : true;
+const byDates = (start?: string, end?: string) => (item: EncodedSeriLogObject) => {
+    if (!start && !end) return true;
+    let res = true;
+    const date = parseJSON(item.timestamp);
+    if (start) {
+        const ds = parseISO(start);
+        res = isAfter(date, ds);
+    }
+    if (end) {
+        const de = parseISO(end);
+        res = isBefore(date, de);
+    }
+    return res;
+}
+const bySearch = (search: string) => (item: EncodedSeriLogObject) => search ? item.message.search(search) : true;
+const byDirection = (direction: string) => (item1: EncodedSeriLogObject, item2: EncodedSeriLogObject) => {
+    const first = parseJSON(item1.timestamp);
+    const second = parseJSON(item2.timestamp);
+    return direction === 'desc' ? compareDesc(first, second) : compareAsc(first, second);
+}
+const applyLimits = (limit: number, page: number) => ({ start: limit * page - limit, end: (limit * page) })
--- src/Serilog.Ui.Web/assets/types/types.ts
+++ src/Serilog.Ui.Web/assets/types/types.ts
@@ -1,4 +1,5 @@
 ﻿export { }
+
 declare global {
     export interface JQuery {
         netStack: ({ prettyprint }: { prettyprint: boolean }) => void
@@ -21,6 +22,52 @@ export enum AuthType {
     Windows = 'Windows'
 }
 
-export type SeriLogObject = {
+export enum LogLevel {
+    Verbose = "Verbose",
+    Debug = "Debug",
+    Information = "Information",
+    Warning = "Warning",
+    Error = "Error",
+}
 
+export enum LogType {
+    Json = "json",
+    Xml = "xml",
+}
+
+export type SeriLogObject = {
+    rowNo: number,
+    level: LogLevel,
+    message: string,
+    timestamp: string,
+    exception?: { [index: string]: string },
+    properties?: { [index: string]: string },
+    propertyType: LogType,
+}
+
+export type EncodedSeriLogObject = {
+    rowNo: number,
+    level: string,
+    message: string,
+    timestamp: string,
+    exception?: string,
+    properties?: string,
+    propertyType: string,
+}
+
+export type SearchResult = {
+    logs: EncodedSeriLogObject[],
+    total: number,
+    count: number,
+    currentPage: number
+}
+
+export enum SearchParameters {
+    Count = 'count',
+    Page = 'page',
+    Level = 'level',
+    Search = 'search',
+    StartDate = 'startDate', // wip
+    EndDate = 'endDate', // wip
+    SortDirection = 'sort' // wip
 }
