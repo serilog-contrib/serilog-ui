897bc2ac6a9420951cb268d4d06df940f08b12c0
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2022-01-09 12:55:53 +0100

    add sort option on the frontend

--- src/Serilog.Ui.Web/assets/script/mocks/fetchMock.ts
+++ src/Serilog.Ui.Web/assets/script/mocks/fetchMock.ts
@@ -1,62 +1,90 @@
-﻿import { isAfter, isBefore, parseJSON, compareAsc, parseISO, compareDesc } from 'date-fns';
-import { rest } from 'msw'
-import { EncodedSeriLogObject, LogLevel, SearchParameters } from '../../types/types';
+﻿import {
+  isAfter,
+  isBefore,
+  parseJSON,
+  compareAsc,
+  parseISO,
+  compareDesc,
+} from 'date-fns';
+import { rest } from 'msw';
+import {
+  EncodedSeriLogObject,
+  LogLevel,
+  SearchParameters,
+  SearchSortDirectionOptions,
+  SearchSortParametersOptions,
+} from '../../types/types';
 import { fakeLogs } from './samples';
 
-const developmentListenersHost = ["127.0.0.1", "localhost"];
+const developmentListenersHost = ['127.0.0.1', 'localhost'];
 export const handlers = [
-    ...developmentListenersHost.map(dlh =>
-        rest.get(`https://${dlh}:1234/api/logs`, (req, res, ctx) => {
-            const params = getSearchParams(req.url.searchParams);
-            const logs = fakeLogs.logs.filter(byLevel(LogLevel[params.level]))
-                .filter(byDates(params.start, params.end))
-                .filter(bySearch(params.search))
-                .sort(byDirection(params.sort));
-            const sliceValues = applyLimits(params.count, params.page);
+  ...developmentListenersHost.map((dlh) =>
+    rest.get(`https://${dlh}:1234/api/logs`, (req, res, ctx) => {
+      const params = getSearchParams(req.url.searchParams);
+      const logs = fakeLogs.logs
+        .filter(byLevel(LogLevel[params.level]))
+        .filter(byDates(params.start, params.end))
+        .filter(bySearch(params.search))
+        .sort(byDirection(params.sorton, params.sortby));
+      const sliceValues = applyLimits(params.count, params.page);
 
-            const data = {
-                currentPage: params.page,
-                count: params.count,
-                total: logs.length,
-                logs: logs.slice(sliceValues.start, sliceValues.end)
-            };
-            return res(ctx.status(200), ctx.json(data))
-        })
-    ),
+      const data = {
+        currentPage: params.page,
+        count: params.count,
+        total: logs.length,
+        logs: logs.slice(sliceValues.start, sliceValues.end),
+      };
+      return res(ctx.status(200), ctx.json(data));
+    }),
+  ),
 ];
 
 const getSearchParams = (params: URLSearchParams) => ({
-    count: Number.parseInt(params.get(SearchParameters.Count)),
-    page: Number.parseInt(params.get(SearchParameters.Page)),
-    level: params.get(SearchParameters.Level),
-    search: params.get(SearchParameters.Search),
-    start: params.get(SearchParameters.StartDate),
-    end: params.get(SearchParameters.EndDate),
-    sort: params.get(SearchParameters.SortDirection),
+  count: Number.parseInt(params.get(SearchParameters.Count)),
+  page: Number.parseInt(params.get(SearchParameters.Page)),
+  level: params.get(SearchParameters.Level),
+  search: params.get(SearchParameters.Search),
+  start: params.get(SearchParameters.StartDate),
+  end: params.get(SearchParameters.EndDate),
+  sorton: params.get(SearchParameters.SortParameter),
+  sortby: params.get(SearchParameters.SortDirection),
 });
 
-const byLevel = (level?: LogLevel) =>
-    (item: EncodedSeriLogObject) => level ? item.level === level : true;
-const byDates = (start?: string, end?: string) => (item: EncodedSeriLogObject) => {
+const byLevel = (level?: LogLevel) => (item: EncodedSeriLogObject) =>
+  level ? item.level === level : true;
+const byDates =
+  (start?: string, end?: string) => (item: EncodedSeriLogObject) => {
     if (!start && !end) return true;
     let after = true;
     let before = true;
 
     const date = parseJSON(item.timestamp);
     if (start) {
-        const ds = parseISO(start);
-        after = isAfter(date, ds);
+      const ds = parseISO(start);
+      after = isAfter(date, ds);
     }
     if (end) {
-        const de = parseISO(end);
-        before = isBefore(date, de);
+      const de = parseISO(end);
+      before = isBefore(date, de);
     }
     return after && before;
-}
-const bySearch = (search: string) => (item: EncodedSeriLogObject) => search ? item.message.toLowerCase().search(search.toLowerCase()) > -1 : true;
-const byDirection = (direction: string) => (item1: EncodedSeriLogObject, item2: EncodedSeriLogObject) => {
+  };
+const bySearch = (search: string) => (item: EncodedSeriLogObject) =>
+  search ? item.message.toLowerCase().search(search.toLowerCase()) > -1 : true;
+const byDirection =
+  (on: string, direction: string) =>
+  (item1: EncodedSeriLogObject, item2: EncodedSeriLogObject) => {
     const first = parseJSON(item1.timestamp);
     const second = parseJSON(item2.timestamp);
-    return direction === 'desc' ? compareDesc(first, second) : compareAsc(first, second);
-}
-const applyLimits = (limit: number, page: number) => ({ start: limit * page - limit, end: (limit * page) })
+    if (on !== SearchSortParametersOptions.UtcTimeStamp) {
+      console.warn('Mock filter not implemented!');
+      return 1;
+    }
+    return direction === SearchSortDirectionOptions.Desc
+      ? compareDesc(first, second)
+      : compareAsc(first, second);
+  };
+const applyLimits = (limit: number, page: number) => ({
+  start: limit * page - limit,
+  end: limit * page,
+});
