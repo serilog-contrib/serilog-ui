c8f0d573e6938c56dce572987fef951f95a6070d
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2022-02-06 12:49:16 +0100

    fix(config): encode-decode the json auth options

--- src/Serilog.Ui.Web/SerilogUiMiddleware.cs
+++ src/Serilog.Ui.Web/SerilogUiMiddleware.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using Serilog.Ui.Core;
 using System;
@@ -127,9 +128,9 @@ namespace Serilog.Ui.Web
 
             await using var stream = IndexStream();
             var htmlBuilder = new StringBuilder(await new StreamReader(stream).ReadToEndAsync());
-            htmlBuilder.Replace("%(Configs)", JsonConvert.SerializeObject(
-                new { _options.RoutePrefix, _options.AuthType }, _jsonSerializerOptions));
-
+            var encodeAuthOpts = Uri.EscapeDataString(JsonConvert.SerializeObject(new { _options.RoutePrefix, _options.AuthType }, _jsonSerializerOptions));
+            htmlBuilder.Replace("%(Configs)", encodeAuthOpts);
+            
             await response.WriteAsync(htmlBuilder.ToString(), Encoding.UTF8);
         }
 
