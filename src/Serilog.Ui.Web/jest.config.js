1a18fe4386d7068e26df299302e7a91d493cc73f
matteoGregoricchio <32459930+followynne@users.noreply.github.com>
2023-03-20 22:43:20 +0100

    fix(build): update sonarscans data

--- src/Serilog.Ui.Web/jest.config.js
+++ src/Serilog.Ui.Web/jest.config.js
@@ -6,5 +6,6 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/assets/__tests__/util/'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[aajt]sx?$',
   collectCoverage: true,
+  collectCoverageFrom: ['**/assets/**/*.ts', '!**/__tests__/**'],
   coverageReporters: ['json', 'html', 'lcov'],
 };
