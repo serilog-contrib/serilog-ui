9cd0b484dd6d3563ff18105d180ef8067c6bc2a2
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2023-03-28 22:51:36 +0200

    fix(tests-elastic): creation of fake data

--- tests/Serilog.Ui.Common.Tests/DataSamples/ElasticSearchLogModelFaker.cs
+++ tests/Serilog.Ui.Common.Tests/DataSamples/ElasticSearchLogModelFaker.cs
@@ -11,10 +11,10 @@ namespace Serilog.Ui.Common.Tests.DataSamples
         {
             var logs = new List<LogModel>();
             logger.Information("90 MyTestSearchItem");
-            logs.Add(Spawn("MyTestSearchItem", 90));
+            logs.Add(Spawn("Information", 90, "MyTestSearchItem"));
             Task.Delay(2000).Wait();
             logger.Information("91 AnotherProp");
-            logs.Add(Spawn("AnotherProp", 91));
+            logs.Add(Spawn("Information", 91, "AnotherProp"));
             Task.Delay(1000).Wait();
             logger.Information("92 Information");
             logs.Add(Spawn("Information", 92));
@@ -38,12 +38,12 @@ namespace Serilog.Ui.Common.Tests.DataSamples
             return new LogModelPropsCollector(logs);
         }
 
-        private static LogModel Spawn(string level, int rowNum)
+        private static LogModel Spawn(string level, int rowNum, string messageOverride = null)
             => new()
             {
                 Exception = null,
                 Level = level,
-                Message = $"{rowNum} {level}",
+                Message = $"{rowNum} {messageOverride ?? level}",
                 Properties = PropertiesFaker.SerializedProperties,
                 PropertyType = "json",
                 RowNo = rowNum,
