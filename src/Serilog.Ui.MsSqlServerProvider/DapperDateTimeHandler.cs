b16f0bad922b790a1924690c0cd494334e5e7671
mesmailpour-spectra <mohsen@projectspectra.dev>
2023-05-28 22:58:04 +0200

    Fix bug converting TimeStamp column to DateTime #62.

---
+++ src/Serilog.Ui.MsSqlServerProvider/DapperDateTimeHandler.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Globalization;
+
+namespace Serilog.Ui.MsSqlServerProvider;
+
+public class DapperDateTimeHandler : SqlMapper.TypeHandler<DateTime>
+{
+    private static string[] Formats = new[] {
+        "M/d/yyyy h:mm:ss tt zzz",
+        "M/dd/yyyy h:mm:ss tt zzz",
+        "M/d/yyyy h:mm:ss tt",
+        "M/dd/yyyy h:mm:ss tt",
+        "M/dd/yyyy hh:mm:ss",
+        "MM/dd/yyyy HH:mm:ss",
+        "MM/d/yyyy hh:mm:ss tt",
+        "M/dd/yyyy hh:mm:ss tt",
+        "MM/dd/yyyy hh:mm:ss tt",
+    };
+
+    public override void SetValue(IDbDataParameter parameter, DateTime value)
+    {
+        parameter.Value = value;
+    }
+
+    public override DateTime Parse(object value)
+    {
+        var valueStr = value.ToString();
+        DateTime.TryParse(valueStr, out var timeStamp);
+
+        if (timeStamp == default)
+            DateTime.TryParseExact(valueStr, Formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out timeStamp);
+
+        return timeStamp;
+    }
+}
