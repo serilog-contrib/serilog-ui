f4c552b1e0d25f1b268776702b3e0cbb243d8afe
matteoGregoricchio <32459930+followynne@users.noreply.github.com>
2023-09-16 09:22:04 +0200

    Update AppStreamLoader.cs

--- src/Serilog.Ui.Web/Endpoints/AppStreamLoader.cs
+++ src/Serilog.Ui.Web/Endpoints/AppStreamLoader.cs
@@ -10,6 +10,7 @@ namespace Serilog.Ui.Web.Endpoints
     internal class AppStreamLoader : IAppStreamLoader
     {
         private const string AppManifest = "Serilog.Ui.Web.wwwroot.dist.index.html";
+
         public Stream GetIndex() =>
             typeof(AuthorizationOptions)
             .Assembly
