8f3c7990ef074dc5bbd7b862c77fd3fb717e6bb8
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2023-11-12 17:22:20 +0100

    migrate to vite

---
+++ src/Serilog.Ui.Web/vite.config.ts
@@ -0,0 +1,49 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint';
+import mkcert from 'vite-plugin-mkcert';
+import { checker } from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig((env) => ({
+  root: './assets',
+  build: {
+    outDir: './wwwroot/dist',
+    sourcemap: false,
+    rollupOptions: {
+      external: ['**/__tests__/**/*', '**/*.{spec,test}.*', '**/mocks/**/*.*'],
+    },
+  },
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    env.mode !== 'test' && eslint(),
+    mkcert(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  server: {
+    https: true,
+    open: false,
+    port: 3001,
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    restoreMocks: true,
+    setupFiles: './src/setupTests.ts',
+    outputFile: {
+      'vitest-sonar-reporter': './app/test-report.xml',
+    },
+    coverage: {
+      provider: 'istanbul',
+      reporter: 'lcov',
+      reportsDirectory: './app/coverage/',
+      reportOnFailure: true,
+    },
+  },
+}));
