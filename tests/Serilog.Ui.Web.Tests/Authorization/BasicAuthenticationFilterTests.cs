f2bb04373039c3ef3a59c7b761d9ac96e4a24f2f
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2023-11-13 00:19:59 +0100

    Squash from master

---
+++ tests/Serilog.Ui.Web.Tests/Authorization/BasicAuthenticationFilterTests.cs
@@ -0,0 +1,74 @@
+using System.Linq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Serilog.Ui.Web.Authorization.Tests;
+
+public class BasicAuthenticationFilterTests
+{
+    [Fact]
+    public async Task Authorize_WithValidCredentials_ShouldReturnTrue()
+    {
+        // Arrange
+        var filter = new BasicAuthenticationFilter
+        {
+            UserName = "User",
+            Password = "P@ss"
+        };
+
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "Basic VXNlcjpQQHNz"; // Base64 encoded "User:P@ss"
+
+        // Act
+        var result = filter.Authorize(httpContext);
+        var authCookie = httpContext.Response.GetTypedHeaders().SetCookie.FirstOrDefault(sc => sc.Name == BasicAuthenticationFilter.AuthenticationCookieName);
+
+        // Assert
+        result.Should().BeTrue();
+        authCookie.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task Authorize_WithInvalidCredentials_ShouldReturnFalse()
+    {
+        // Arrange
+        var filter = new BasicAuthenticationFilter
+        {
+            UserName = "User",
+            Password = "P@ss"
+        };
+
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers["Authorization"] = "Basic QWRtaW46dXNlcg=="; // Base64 encoded "Admin:user"
+
+        // Act
+        var result = filter.Authorize(httpContext);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Authorize_WithMissingAuthorizationHeader_ShouldSetChallengeResponse()
+    {
+        // Arrange
+        var filter = new BasicAuthenticationFilter
+        {
+            UserName = "User",
+            Password = "P@ss"
+        };
+
+        var httpContext = new DefaultHttpContext();
+
+        // Act
+        var result = filter.Authorize(httpContext);
+
+        // Assert
+        result.Should().BeFalse();
+        httpContext.Response.StatusCode.Should().Be(401);
+        httpContext.Response.Headers[HeaderNames.WWWAuthenticate].Should().Contain("Basic realm=\"Serilog UI\"");
+    }
+}
