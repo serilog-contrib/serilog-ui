8d7977748f56416825bfcdf4e56bb3bb77718304
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2024-01-03 23:16:21 +0100

    replace xml formatting, prevent double format

--- src/Serilog.Ui.Web/assets/script/util.ts
+++ src/Serilog.Ui.Web/assets/script/util.ts
@@ -4,19 +4,6 @@ import { LogLevel } from '../types/types';
 export const formatDate = (date: string) =>
   format(parseJSON(date), 'PP H:mm:ss.SSS', { weekStartsOn: 1 });
 
-export const formatXml = (xml: string, tab = '\t') => {
-  let formatted = '',
-    indent = '';
-  xml.split(/>\s*</).forEach(function (node) {
-    // decrease indent by one "tab"
-    if (node.match(/^\/\w/)) indent = indent.substring(tab.length);
-    formatted += indent + '<' + node + '>\r\n';
-    // increase indent
-    if (node.match(/^<?\w[^>]*[^\/]$/)) indent += tab;
-  });
-  return formatted.substring(1, formatted.length - 3);
-};
-
 export const fixedLengthMessageWithModal = (str: string, sliceEnd: number) => {
   if (str.length <= sliceEnd) {
     return str;
