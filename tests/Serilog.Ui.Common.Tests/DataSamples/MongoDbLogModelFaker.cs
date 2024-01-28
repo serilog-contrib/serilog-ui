16731d5ab115243226b5103226d64db30f2d35ea
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2024-01-28 15:43:08 +0100

    restore wrong usage of serializer

--- tests/Serilog.Ui.Common.Tests/DataSamples/MongoDbLogModelFaker.cs
+++ tests/Serilog.Ui.Common.Tests/DataSamples/MongoDbLogModelFaker.cs
@@ -3,7 +3,7 @@ using Serilog.Ui.Common.Tests.FakeObjectModels;
 using Serilog.Ui.MongoDbProvider;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
+using Newtonsoft.Json;
 using Bogus;
 
 namespace Serilog.Ui.Common.Tests.DataSamples
@@ -18,7 +18,7 @@ namespace Serilog.Ui.Common.Tests.DataSamples
             var modelCollector = new LogModelPropsCollector(originalLogs);
 
             var faker = new Faker();
-            
+
             var logs = originalLogs.Select(p => new MongoDbLogModel
             {
                 Id = p.RowNo,
@@ -26,7 +26,7 @@ namespace Serilog.Ui.Common.Tests.DataSamples
                 RenderedMessage = p.Message,
                 Timestamp = p.Timestamp,
                 UtcTimeStamp = p.Timestamp.ToUniversalTime(),
-                Properties = JsonSerializer.Deserialize<Properties>(p.Properties),
+                Properties = JsonConvert.DeserializeObject<Properties>(p.Properties),
                 Exception = faker.System.Exception() // Serialization round-trip not possible for an exception, so we generate a new exception.
                     .ToBsonDocument(),
             });
