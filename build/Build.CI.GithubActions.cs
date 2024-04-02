7bc8f55216f7bec0a665fffad027ba8e08054acf
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2024-04-02 22:47:50 +0200

    test

--- build/Build.CI.GithubActions.cs
+++ build/Build.CI.GithubActions.cs
@@ -78,7 +78,7 @@ partial class Build
 
     Target Backend_SonarScan_Start => targetDefinition => targetDefinition
         .DependsOn(Backend_Restore)
-        .OnlyWhenStatic(() => OnGithubActionRun && !IsPr &&
+        .OnlyWhenStatic(() => OnGithubActionRun &&
             !string.IsNullOrWhiteSpace(SonarCloudInfo.Organization) &&
             !string.IsNullOrWhiteSpace(SonarCloudInfo.BackendProjectKey)
         )
@@ -107,7 +107,7 @@ partial class Build
 
     Target Backend_SonarScan_End => targetDefinition => targetDefinition
         .DependsOn(Backend_Test_Ci)
-        .OnlyWhenStatic(() => OnGithubActionRun && !IsPr &&
+        .OnlyWhenStatic(() => OnGithubActionRun &&
             !string.IsNullOrWhiteSpace(SonarCloudInfo.Organization) &&
             !string.IsNullOrWhiteSpace(SonarCloudInfo.BackendProjectKey)
         )
