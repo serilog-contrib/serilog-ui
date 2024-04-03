eb1244a01b04d31e29c0ed66df1b8b9a6621e6e6
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2024-04-03 14:32:43 +0200

    forgotten file

--- src/Serilog.Ui.Web/src/app/hooks/useJwtTimeout.ts
+++ src/Serilog.Ui.Web/src/app/hooks/useJwtTimeout.ts
@@ -9,28 +9,28 @@ import { useSerilogUiProps } from './useSerilogUiProps';
  */
 export function useJwtTimeout(): void {
   const MINUTE = 60000;
-  const [errorSet, stopErrorCheck] = useState(false);
+  const [stopError, setStopError] = useState(false);
   const { authType } = useSerilogUiProps();
   const { jwt_bearerToken } = useAuthProperties();
 
   // when token changes, validation can resume
   useEffect(() => {
-    stopErrorCheck(false);
+    setStopError(false);
   }, [jwt_bearerToken]);
 
   useEffect(() => {
     // if error was already spotted, we don't start checking until jwt is changed
-    if (authType !== AuthType.Jwt || !jwt_bearerToken || errorSet) return;
+    if (authType !== AuthType.Jwt || !jwt_bearerToken || stopError) return;
 
     const id = setTimeout(() => {
       const result = validateKey('jwt_bearerToken', jwt_bearerToken);
 
       checkErrors(result);
-      if (result.error) stopErrorCheck(true);
+      if (result.error) setStopError(true);
     }, MINUTE);
 
     return () => {
       clearTimeout(id);
     };
-  }, [authType, errorSet, jwt_bearerToken]);
+  }, [authType, stopError, jwt_bearerToken]);
 }
