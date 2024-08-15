3ddec088c366ac89838744094fac5b16527a4b24
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2024-08-15 12:23:41 +0200

    tests(mongodb): use real sink to generate logs

--- tests/Serilog.Ui.MongoDbProvider.Tests/DataProvider/DataProviderSearchTest.cs
+++ tests/Serilog.Ui.MongoDbProvider.Tests/DataProvider/DataProviderSearchTest.cs
@@ -1,11 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Microsoft.Extensions.Primitives;
-using MongoDb.Tests.Util;
+﻿using MongoDb.Tests.Util;
 using Serilog.Ui.Common.Tests.TestSuites.Impl;
-using Serilog.Ui.Core.Models;
 using Serilog.Ui.MongoDbProvider;
 using Xunit;
 
@@ -13,31 +7,5 @@ namespace MongoDb.Tests.DataProvider
 {
     [Collection(nameof(MongoDbDataProvider))]
     [Trait("Integration-Search", "MongoDb")]
-    public class DataProviderSearchTest(BaseIntegrationTest instance) : IntegrationSearchTests<BaseIntegrationTest>(instance)
-    {
-        public override async Task It_finds_only_data_emitted_in_dates_range()
-        {
-            var firstTimeStamp = LogCollector.TimesSamples.First().AddSeconds(50);
-            var lastTimeStamp = LogCollector.TimesSamples.Last().AddSeconds(50);
-            var inTimeStampCount = LogCollector.DataSet
-                .Count(p => p.Timestamp >= firstTimeStamp && p.Timestamp < lastTimeStamp);
-            var query = new Dictionary<string, StringValues>
-            {
-                ["page"] = "1",
-                ["count"] = "1000",
-                ["startDate"] = firstTimeStamp.ToString("O"),
-                ["endDate"] = lastTimeStamp.ToString("O")
-            };
-
-            var (logs, count) = await Provider.FetchDataAsync(FetchLogsQuery.ParseQuery(query));
-
-            var enumerateLogs = logs.ToList();
-            enumerateLogs.Should().NotBeEmpty();
-            enumerateLogs.Should().HaveCount(inTimeStampCount);
-            count.Should().Be(inTimeStampCount);
-            enumerateLogs.Should().OnlyContain(p =>
-                p.Timestamp.ToUniversalTime() >= firstTimeStamp &&
-                p.Timestamp.ToUniversalTime() < lastTimeStamp);
-        }
-    }
+    public class DataProviderSearchTest(BaseIntegrationTest instance) : IntegrationSearchTests<BaseIntegrationTest>(instance);
 }
