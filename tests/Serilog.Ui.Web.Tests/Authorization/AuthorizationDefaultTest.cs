df9a2aa6c5edbba427e003df10edaf66d11091c9
Matteo Gregoricchio <mtt-safe-grgo@outlook.com>
2026-04-26 21:16:32 +0200

    improve web-server test, change sqlite targets

--- tests/Serilog.Ui.Web.Tests/Authorization/AuthorizationDefaultTest.cs
+++ tests/Serilog.Ui.Web.Tests/Authorization/AuthorizationDefaultTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Net.Http;
+﻿using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
+using NSubstitute;
 using Serilog.Ui.Web.Tests.Utilities;
 using Xunit;
 
@@ -14,6 +16,9 @@ public class AuthorizationDefaultTest(WebAppFactory.Default factory) : IClassFix
     [Fact]
     public async Task Local_Requests_Are_Allowed_By_Default()
     {
+        // Arrange
+        factory.appStreamLoader.GetIndex().Returns(new MemoryStream());
+
         // Act
         var response = await _client.GetAsync("/serilog-ui/index.html");
         var result = response.EnsureSuccessStatusCode;
